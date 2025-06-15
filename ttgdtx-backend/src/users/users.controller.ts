c5952aa1b886804b5a170f93afd5e71e6183c4e1
HieuCuteDangYeu <nguyendinhhieu050505@gmail.com>
2025-06-15 17:01:01 +0700

    refactor and add auth for users api

--- ttgdtx-backend/src/users/users.controller.ts
+++ ttgdtx-backend/src/users/users.controller.ts
@@ -6,29 +6,39 @@ import {
   Param,
   Patch,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Types } from 'mongoose';
 import { MongoIdPipe } from 'src/pipes/mongo-id.pipe';
 import { PaginationQueryDto } from './dto/pagination-query.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthenticationGuard } from 'src/guards/authentication.guard';
+import { AuthorizationGuard } from 'src/guards/authorization.guard';
+import { Action } from 'src/roles/enums/action.enum';
+import { Resource } from 'src/roles/enums/resource.enum';
+import { Permissions } from 'src/decorators/permissions.decorator';
 
+@UseGuards(AuthenticationGuard, AuthorizationGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  async findAll(@Query() paginationQuery: PaginationQueryDto) {
+  @Permissions([{ resource: Resource.users, actions: [Action.read] }])
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
     const { page = 1, limit = 10 } = paginationQuery;
     return this.usersService.findAll({ page, limit });
   }
 
   @Get(':id')
+  @Permissions([{ resource: Resource.users, actions: [Action.read] }])
   findOne(@Param('id', MongoIdPipe) id: Types.ObjectId) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @Permissions([{ resource: Resource.users, actions: [Action.update] }])
   update(
     @Param('id', MongoIdPipe) id: Types.ObjectId,
     @Body() updateUserDto: UpdateUserDto,
@@ -37,7 +47,8 @@ export class UsersController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: Types.ObjectId) {
+  @Permissions([{ resource: Resource.users, actions: [Action.delete] }])
+  remove(@Param('id', MongoIdPipe) id: Types.ObjectId) {
     return this.usersService.remove(id);
   }
 }
--- ttgdtx-backend/src/users/users.service.ts
+++ ttgdtx-backend/src/users/users.service.ts
@@ -1,75 +1,28 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { User, UserDocument } from './schemas/user.schema';
 import { Model, Types } from 'mongoose';
+import { BaseService } from 'src/common/base.service';
+import { UserDocument, User } from './schemas/user.schema';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-export interface PaginationOptions {
-  page: number;
-  limit: number;
-}
-
-export interface PaginatedUsers {
-  users: User[];
-  total: number;
-  page: number;
-  limit: number;
-  totalPages: number;
-}
-
 @Injectable()
-export class UsersService {
-  constructor(
-    @InjectModel(User.name)
-    private userModel: Model<UserDocument>,
-  ) {}
-
-  async findAll(options: PaginationOptions): Promise<PaginatedUsers> {
-    const { page, limit } = options;
-    const skip = (page - 1) * limit;
-
-    const [results, total] = await Promise.all([
-      this.userModel.find().skip(skip).limit(limit).exec(),
-      this.userModel.countDocuments().exec(),
-    ]);
-
-    const totalPages = Math.ceil(total / limit);
-
-    return {
-      users: results,
-      total,
-      page,
-      limit,
-      totalPages,
-    };
+export class UsersService extends BaseService<UserDocument> {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {
+    super(userModel);
   }
 
-  async findOne(id: Types.ObjectId): Promise<User> {
-    const user = await this.userModel.findById(id).exec();
-    if (!user) {
-      throw new Error(`User with ID ${id.toString()} not found.`);
-    }
-    return user;
-  }
-
-  async update(
+  protected async validateUpdate(
     id: Types.ObjectId,
-    updateUserDto: UpdateUserDto,
-  ): Promise<User> {
-    const existingUser = await this.userModel
-      .findByIdAndUpdate(id, updateUserDto, { new: true })
-      .exec();
-    if (!existingUser) {
-      throw new NotFoundException(`User with ID "${id.toString()}" not found.`);
+    updateDto: UpdateUserDto,
+  ): Promise<void> {
+    if (updateDto.email) {
+      const existingUser = await this.userModel.findOne({
+        email: updateDto.email,
+        _id: { $ne: id },
+      });
+      if (existingUser) {
+        throw new BadRequestException('Email already exists');
+      }
     }
-    return existingUser;
-  }
-
-  async remove(id: Types.ObjectId): Promise<any> {
-    const result = await this.userModel.deleteOne({ _id: id }).exec();
-    if (result.deletedCount === 0) {
-      throw new NotFoundException(`User with ID "${id.toString()}" not found.`);
-    }
-    return { message: `User with ID "${id.toString()}" successfully removed.` };
   }
 }
